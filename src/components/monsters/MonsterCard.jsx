c8b36fa392643bf7fb8fce84b712e74675d01bd4
Shaun <scpour@gmail.com>
2023-12-15 12:41:06 -0500

    Set up map to input required properties into the monstercard component and print all of them to the page

--- src/components/monsters/MonsterCard.jsx
+++ src/components/monsters/MonsterCard.jsx
@@ -2,38 +2,37 @@ import { Component } from "react";
 import { Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 
 export default class MonsterCard extends Component {
+  state = {
+    name: '',
+    type: '',
+    species: '',
+    description: ''
+  };
+
+  componentDidMount() {
+    const { name, type, species, description } = this.props;
+    this.setState({name, type, species, description});
+  }
+
   render() {
     return (
-        <div className="col-md-4 col-sm-6 mb-5">
-        <Card
-  style={{
-    width: '95%'
-  }}
-><img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-    <CardHeader>
-    <CardTitle tag="h5">
-      Card title
-    </CardTitle>
-  </CardHeader>
-  <CardBody>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      Card subtitle
-    </CardSubtitle>
-    <CardText>
-      Some quick example text to build on the card title and make up the bulk of the card‘s content.
-    </CardText>
-    <Button>
-      Button
-    </Button>
-  </CardBody>
-</Card>
-</div>
+      <div className="col-md-4 col-sm-6 mb-5">
+        <Card style={{width: '95%'}}>
+          <CardHeader>
+          <CardTitle tag="h5">
+            {this.state.name}
+          </CardTitle>
+        </CardHeader>
+        <CardBody>
+          <div className="d-flex align-items-center justify-content-center">
+          <CardSubtitle className="mb-2 text-muted text-capitalize p-4" tag="h6">{this.state.type}</CardSubtitle>
+          <CardSubtitle className="mb-2 text-muted text-capitalize" tag="h6">{this.state.species}</CardSubtitle>
+          </div>
+          <CardText>{this.state.description}</CardText>
+            <Button>See more details</Button>
+          </CardBody>
+        </Card>
+      </div>
     )
   }
 }
--- src/components/monsters/MonsterList.jsx
+++ src/components/monsters/MonsterList.jsx
@@ -1,33 +1,28 @@
+import axios from "axios";
 import { Component } from "react";
-import MonsterCard from '../monsters/monsterCard';
 import { CardGroup } from "reactstrap";
+import spinner from './spinner.gif';
+import MonsterCard from '../monsters/monsterCard';
 
 export default class MonsterList extends Component {
     state = {
-        url: 'https://mhw-db.com/monsters'
+        url: 'https://mhw-db.com/monsters',
+        monster: null
     };
+
+    async componentDidMount() {
+        const res = await axios.get(this.state.url);
+        this.setState({ monster: res.data });
+    }
+
   render() {
     return (
         <CardGroup className="monsterList">
-            <div className="row">
-                <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            <MonsterCard />
-            </div>
-            
+            {this.state.monster ? (<div className="row">
+            {this.state.monster.map(monster => (
+            <MonsterCard key={monster.name} name={monster.name} type={monster.type} species={monster.species} description={monster.description} />
+        ))}
+            </div>) : <img src={spinner} style={{width:'5em', height: '5em'}} className="card-img-top rounded mx-auto d-block mt-2" />}
         </CardGroup>
     )
   }
